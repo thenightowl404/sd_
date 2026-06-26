5c7ddf9c34e8f44ab316164dabb6efc13a39c516
thenightowl404 <humaira2412005@gmail.com>
2026-06-26 15:56:47 +0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'core',
+    
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = []
